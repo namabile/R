6e8666e722936da24136f5c204ebc60deccce7c6
Nick Amabile <amabile.n@gmail.com>
2012-03-07 18:51:51 -0800

    change cart conversion to use visits

--- cart conversion test 3-6-12.r
+++ cart conversion test 3-6-12.r
@@ -8,11 +8,12 @@ source("x:/ad hoc/R/lib/lib.r")
 ga <- GA()
 ga$GetProfileIDs()
 
-start_date <- mdy("3/6/2012")
+#start_date <- mdy("3/6/2012")
+start_date <- mdy("3/7/2012")
 end_date <- ymd(toString(Sys.Date()))
 
 dimensions <- c("ga:eventCategory,ga:eventLabel")
-metrics <- c("ga:visits,ga:totalEvents,ga:uniqueEvents")
+metrics <- c("ga:visits,ga:totalEvents,ga:uniqueEvents,ga:transactions")
 filters <- c("ga:eventCategory==Checkout,ga:eventCategory==Confirmation")
 sort <- c("ga:eventLabel")
 
@@ -30,19 +31,13 @@ result <- ga$GetReportData(query)$data
 ga.data <- rbind(ga.data, result)
 
 names <- factor(ga.data$"ga:eventCategory")
-new_cart <- ga.data[ga.data$"ga:eventLabel" == "New", "ga:totalEvents"]
-names(new_cart) <- levels(names)
-old_cart <- ga.data[ga.data$"ga:eventLabel" == "Old", "ga:totalEvents"]
-names(old_cart) <- levels(names)
+new_cart <- ga.data[ga.data$"ga:eventLabel" == "New",]
+old_cart <- ga.data[ga.data$"ga:eventLabel" == "Old",]
 
-new_conversion <- new_cart["Confirmation"]/new_cart["Checkout"] * 100
-old_conversion <- old_cart["Confirmation"]/old_cart["Checkout"] * 100
+new_conversion <- new_cart[ga.data$"ga:eventCategory" == "Confirmation","ga:transactions"]/new_cart[ga.data$"ga:eventCategory" == "Checkout", "ga:visits"] * 100
+old_conversion <- old_cart[ga.data$"ga:eventCategory" == "Confirmation","ga:transactions"]/old_cart[ga.data$"ga:eventCategory" == "Checkout", "ga:visits"] * 100
 
-new_cart <- c(new_cart,new_conversion)
-old_cart <- c(old_cart,old_conversion)
-
-new_data <- rbind(new_cart,old_cart)
+#new_data <- rbind(new_cart,old_cart)
 var <- (new_conversion/old_conversion - 1) * 100
-colnames(new_data) <- c("Checkout","Confirmation","Conversion")
-print(new_data)
+#colnames(new_data) <- c("Checkout","Confirmation","Conversion")
 print(var)
